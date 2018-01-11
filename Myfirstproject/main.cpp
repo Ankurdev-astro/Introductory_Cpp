e5f2830281390d01e2078aee992017cc9c5d8cc5
ap16032 <a.dev@se16.qmul.ac.uk>
2018-01-11 16:36:24 +0000

    2nd Update

--- Myfirstproject/main.cpp
+++ Myfirstproject/main.cpp
@@ -3,11 +3,16 @@ using namespace std;
 int main() {
     cout << "Hello, World!! \n";
     int a = 7;
+    int b = 8;
     if (a==7)
     {
         cout << a;
     }
-    else
+    else{
         cout << "a is not equal to 7" ;
+        cout << b;
+    }
+
+
     return 0;
 }
