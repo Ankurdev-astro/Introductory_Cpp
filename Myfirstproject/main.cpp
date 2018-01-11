036de64beedb4ec856274749a0d2cc93267c3bf8
ap16032 <a.dev@se16.qmul.ac.uk>
2018-01-11 16:23:45 +0000

    2nd Update

--- Myfirstproject/main.cpp
+++ Myfirstproject/main.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
 using namespace std;
 int main() {
-    cout << "Hello, World!!";
+    cout << "Hello, World!! \n";
+    int a = 7;
+    if (a==7)
+    {
+        cout << a;
+    }
+    else
+        cout << "a is not equal to 7" ;
     return 0;
 }
