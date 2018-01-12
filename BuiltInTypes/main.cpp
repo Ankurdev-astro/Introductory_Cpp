7aa660462cc41fe5bbe7e29215033dfe6b8a5b17
ap16032 <a.dev@se16.qmul.ac.uk>
2018-01-12 16:18:32 +0000

    Section A_2 ongoing

---
+++ BuiltInTypes/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+    int integer = 5;
+    char charac = 'q';
+    float float_num = 6.5;
+    double precision =  16.9376523454;
+    bool correct = true;
+
+    cout << integer << endl << charac << endl << float_num << endl;
+    cout << precision <<endl << correct;
+    return 0;
+
+}
+
