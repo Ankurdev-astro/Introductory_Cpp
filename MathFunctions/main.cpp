1f325b9dbbf15372472b6ceb97f5666521dafced
ap16032 <a.dev@se16.qmul.ac.uk>
2018-01-18 16:34:50 +0000

    Partly completed/Ex6

---
+++ MathFunctions/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#define _USE_MATH_DEFINES
+#include <cmath>
+using namespace std;
+// 4_Exercises-SectionA_2/Exercise 6
+
+int main() {
+    int x= 3, y =4, z =5;
+    float r = sqrt(x*x + y*y);
+    cout << "x= 3, y =4, z =5";
+    cout << "z*(x+y) = " << z*(x+y)<< endl;
+    cout << "4.pi.r^2 = " << 4*M_PI*r*r << endl;
+    // not finished
+
+}
