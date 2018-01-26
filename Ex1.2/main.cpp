30ce683ded33973cb96c2d7af06ebd9dc6ae52e2
ap16032 <a.dev@se16.qmul.ac.uk>
2018-01-26 14:39:21 +0000

    Section B/Hw/Ex2 done

---
+++ Ex1.2/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+// Section B/ Exercise 2
+
+int main() {
+    float f1, f2, f3, f4, f5;
+    //cout << "Enter 5 numbers: \n";
+    cin >> f1;
+    cin >> f2;
+    cin >> f3;
+    cin >> f4;
+    cin >> f5;
+    cout << f1 + f2 + f3 + f4 + f5;
+    return 0;
+}
