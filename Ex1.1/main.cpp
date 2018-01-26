3c57854bc2fb1c1bb1c6b32ca8eaa0da450b88fa
ap16032 <a.dev@se16.qmul.ac.uk>
2018-01-26 14:24:01 +0000

    SectionB/Hw/Ex1.1 done

---
+++ Ex1.1/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+// Section B/ Exercise 1
+
+int main() {
+    string str;
+    //cout << "Enter a word" << endl;
+    cin >> str;
+    cout <<str;
+    return 0;
+}
