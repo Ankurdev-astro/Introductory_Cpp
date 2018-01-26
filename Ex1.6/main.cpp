9c88eb8d9e212b246caabe9a4c3e8a1df9cb0ef8
ap16032 <a.dev@se16.qmul.ac.uk>
2018-01-26 15:14:06 +0000

    Section B/ Exercise 6/ Hw Done

---
+++ Ex1.6/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+//Section B/ Hw/ Exercise 6
+
+int main() {
+    int num,i, n=12,val;
+    //cout << "Enter an integer" << endl ;
+    cin >> num;
+    for(i=1;i<=n;i++)
+    {
+        val = i*num;
+        cout << i << " x " << num << " = " << val <<endl;
+    }
+    return 0;
+}
