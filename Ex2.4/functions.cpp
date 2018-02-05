98e2d4c6acdd74c61ad3bcdc1b094b154ce17fd6
ap16032 <a.dev@se16.qmul.ac.uk>
2018-02-05 14:37:22 +0000

    Section C/ Worksheet 1/ HW/ Ex 4 update

---
+++ Ex2.4/functions.cpp
@@ -0,0 +1,31 @@
+//
+// Created by ap16032 on 05/02/2018.
+//
+
+#include "functions.h"
+#include "Window.h"
+
+void makeInscribedPolygon (SPA::Window & window, int sides, float radius, int x, int y)
+    {
+        float r, theta, n, rad, len;
+        n = sides;
+        r = radius;
+        len = r*(2*sin(M_PI/n));
+        theta = float((n-2)*180)/n;
+        rad = (theta*M_PI)/180;
+        //r = len/(2*sin(M_PI/n));
+        x = x - r*cos(rad/2);
+        y = y - r*sin(rad/2);
+        window.startNewLine(x,y);
+        window.penSetAngle(0);
+
+        for (int i=0;i<len;i++)
+        {
+            window.penForward(len);
+            window.penRotateDegrees(180-theta);
+        }
+
+        window.stopCurrentLine();
+        window.display();
+
+    }
---
+++ Ex2.4/functions.h
@@ -0,0 +1,11 @@
+//
+// Created by ap16032 on 05/02/2018.
+//
+
+#ifndef XXXXXX_FUNCTIONS_H
+#define XXXXXX_FUNCTIONS_H
+
+#include "Window.h"
+
+void makeInscribedPolygon (SPA::Window & window, int sides, float radius, int x, int y);
+#endif //XXXXXX_FUNCwTIONS_H
