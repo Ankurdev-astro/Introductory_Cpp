16f9810d5dc08e0b45fb38896d1d50c8f60ce007
ap16032 <a.dev@se16.qmul.ac.uk>
2018-02-05 13:52:21 +0000

    Section C/ HW/ Sheet1/ Ex3 update

---
+++ Ex2.3/functions.cpp
@@ -0,0 +1,11 @@
+//
+// Created by ap16032 on 05/02/2018.
+//
+
+#include "functions.h"
+#include "Window.h"
+
+void makePolygon(SPA::Window & window, int sides, int length, int x, int y)
+    {
+
+    }
---
+++ Ex2.3/functions.h
@@ -0,0 +1,11 @@
+//
+// Created by ap16032 on 05/02/2018.
+//
+
+#ifndef XXXXXX_FUNCTIONS_H
+#define XXXXXX_FUNCTIONS_H
+
+#include "Window.h"
+
+void makePolygon(SPA::Window & window, int sides, int length, int x, int y);
+#endif //XXXXXX_FUNCTIONS_H
