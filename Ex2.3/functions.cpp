c62997f6dfa2058bc5fdc5e4e53ea09b8da6dd7c
ap16032 <a.dev@se16.qmul.ac.uk>
2018-02-05 14:16:26 +0000

    Section C/ HW/ Sheet1/ Ex3 completed

--- Ex2.3/functions.cpp
+++ Ex2.3/functions.cpp
@@ -7,5 +7,26 @@
 
 void makePolygon(SPA::Window & window, int sides, int length, int x, int y)
     {
+        float r, theta, n, rad, len;
+        n = sides;
+        len = length;
+        theta = float((n-2)*180)/n;
+        rad = (theta*M_PI)/180;
+        r = len/(2*sin(M_PI/n));
+        x = x - r*cos(rad/2);
+        y = y - r*sin(rad/2);
+        window.startNewLine(x,y);
+        window.penSetAngle(0);
+
+        for (int i=0;i<len;i++)
+        {
+            window.penForward(len);
+            window.penRotateDegrees(180-theta);
+        }
+
+        window.stopCurrentLine();
+        window.display();
+
+
 
     }
--- Ex2.3/main.cpp
+++ Ex2.3/main.cpp
@@ -1,10 +1,3 @@
-/*
- * main.cpp
- *
- *  Created on: 18 Dec 2014
- *      Author: apw253
- */
-
 #include "Window.h"
 #include "functions.h"
 
@@ -14,31 +7,11 @@ int main(int argc, char * argv[]) {
 // create a new window of size 500 x 500 pixels
 // the top left corner of the window is (0,0)
 	SPA::Window window(500,500,"My Test");
+    makePolygon(window,4,100,125,125);
+    makePolygon(window,5,100,125,375);
+    makePolygon(window,6,100,375,375);
+    makePolygon(window,7,100,375,125);
 
-	// Add code here to draw things
-
-	/*
-	 * Here are some of the options
-	 * window.startNewLine(float x, float y) start a new line from location (x,y)
-	 * window.penSetAngle(float angle) to set the initial ang of the pen in degrees
-	 * window.penRotateDegrees(float angle) rotate the pen clockwise in degrees
-	 * window.penForward(float distance) move the pen forward
-	 * window.stopLine() to stop the current line
-*/
-
-	/* uncomment the following lines to see a working example of how to draw lines
-	window.startNewLine(0,250);
-	window.penForward(250);
-	window.penRotateDegrees(90);
-	window.penForward(250);
-	window.stopCurrentLine();
-	 */
-
-	// display the window
-	window.display();
-	// run the FLTK event loop - this allows you to move the window around etc
-	// and ultimately set up click events.
-	// program will end when you close the window
 	return Fl::run();
 }
 
