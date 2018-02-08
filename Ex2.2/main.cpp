90a9e003a3742e450af16715e0d01e5fb4764f1b
ap16032 <a.dev@se16.qmul.ac.uk>
2018-02-08 17:29:14 +0000

    Section C/ Worksheet 1/HW/ Ex2 update1

--- Ex2.2/main.cpp
+++ Ex2.2/main.cpp
@@ -1,21 +1,45 @@
 #include "Window.h"
+using namespace std;
 
+// Section C/ Worksheet 1/HW/ Exercise 2
 
+float rad(float deg)
+{
+    float r = (deg*M_PI)/180;
+    return r;
+}
 
 int main(int argc, char * argv[]) {
 // create a new window of size 500 x 500 pixels
 // the top left corner of the window is (0,0)
 	SPA::Window window(500,500,"My Test");
+    int nSides;
+    float sideLength;
+    float n,theta,r,x,y;
+    cout << "Enter the number of sides of shape: \n ";
+    do
+        cin >> nSides;
+    while(nSides<=2);
+    cout << "Enter the side length: \n ";
+    do
+        cin >> sideLength;
+    while(sideLength<=0);
+    n = nSides;
+    theta = float((n-2)*180)/n;
+    r = sideLength/(2*sin(M_PI/n));
+    x = 250 - r*cos(rad(theta/2));
+    y = 250 - r*sin(rad(theta/2));
+    window.startNewLine(x,y);
+    window.penSetAngle(0);
 
+    for (int i=0;i<n;i++)
+    {
+        window.penForward(sideLength);
+        window.penRotateDegrees(180-theta);
+    }
 
+    window.stopCurrentLine();
 
-	/* uncomment the following lines to see a working example of how to draw lines
-	window.startNewLine(0,250);
-	window.penForward(250);
-	window.penRotateDegrees(90);
-	window.penForward(250);
-	window.stopCurrentLine();
-	 */
 
 	// display the window
 	window.display();
