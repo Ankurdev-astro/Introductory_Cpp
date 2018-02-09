09b46ac7a24d1891cf016a4f533749bf23c1fa19
Ankurdev-eagle <ankurdev93@gmail.com>
2018-02-09 23:17:27 +0000

    Reference"&"/Section C/ Pointers/ Exercise1

---
+++ References1/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+// Section C/ Pointers/ Exercise1
+int main() {
+    cout << "Enter an integer: \n";
+    int p;
+    cin >> p;
+    cout << p <<endl;
+    int &x = p; // x is now a reference variable to p
+    cout << &x <<endl; //&x is the address
+    p =  p + 10;
+    cout << p << " " << x;
+
+
+}
