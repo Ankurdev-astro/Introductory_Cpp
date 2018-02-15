1ac35fec128c4dddbe6cf97db38aa886270345c0
Ankur Dev <a.dev@se16.qmul.ac.uk>
2018-02-15 21:58:05 +0000

    (home2)__Section C/ HW/ Worksheet2/ Exercise 7 updatePart A

---
+++ Ex2.7/functions.cpp
@@ -0,0 +1,13 @@
+//
+// Created by ap16032 on 2/15/2018.
+//
+
+#include "functions.h"
+#include <fstream>
+#include <vector>
+#include <iostream>
+
+void readPointsFromStream(int n, std::vector<float> &xvec, std::vector<float> &yvec, std::ifstream &f)
+{
+    std::cout<<n;
+}
---
+++ Ex2.7/functions.h
@@ -0,0 +1,11 @@
+//
+// Created by ap16032 on 2/15/2018.
+//
+
+#ifndef EX2_7_FUNCTIONS_H
+#define EX2_7_FUNCTIONS_H
+#include <fstream>
+#include <vector>
+
+void readPointsFromStream(int n, std::vector<float> &xvec, std::vector<float> &yvec, std::ifstream &f);
+#endif //EX2_7_FUNCTIONS_H
