2469de607ea8a473f4de5075f94ea932f43c19d4
Ankur Dev <a.dev@se16.qmul.ac.uk>
2018-02-16 19:20:02 +0000

    (home2) Section C/ Worksheet 2/ HW/ Exercise 9 completed

--- Ex2.9/main.cpp
+++ Ex2.9/main.cpp
@@ -16,16 +16,20 @@ int main(int argc, char * argv[]) {
 	Fl::visual(FL_DOUBLE | FL_INDEX);
 
 	// Add code here to draw things
-	Shape * shape = new Shape(250,250,4);
-    Shape * shape2 = new Shape(250,250,4);
-	shape->setRotationSpeed(0); // initial rotation speed in degrees
-    shape2->setRotationSpeed(0);
+	Shape * shape = new Shape(250,250,0);
+    Shape * shape2 = new Shape(250,250,0);
+	Shape * shape3 = new Shape(250,250,0);
+	shape->setRotationSpeed(2); // initial rotation speed in degrees
+    shape2->setRotationSpeed(4);
+	shape3->setRotationSpeed(-4);
 	// make a new line
 	Line * line = new Line();
     Line * line2 = new Line();
+	Line * line3 = new Line();
 	// allowed colours are specified in the enum in Colours.hpp
 	line->setColor(getColor(BLACK));
     line2->setColor(getColor(RED));
+	line3->setColor(getColor(BLUE));
 	// add a point to the line
 	// these points are all relative to the centre of the Shape
 	line->addPoint(Point(-100,-100));
@@ -35,19 +39,26 @@ int main(int argc, char * argv[]) {
 	line->addPoint(Point(-100,-100));
 
     //shape 2
-    line2->addPoint(Point(-125,100-(25.0/tan(M_PI/3)))); //-cos(pi/6),
-    line2->addPoint(Point(-75,100-(25.0/tan(M_PI/3))));
-    line2->addPoint(Point(-150,150+50*sin(M_PI/3)-(25.0/tan(M_PI/3))));
-    line2->addPoint(Point(-125,100-(25.0/tan(M_PI/3))));
+    line2->addPoint(Point(-125,-100+(25.0/tan(M_PI/3))));
+    line2->addPoint(Point(-75, -100+(25.0/tan(M_PI/3))));
+    line2->addPoint(Point(-100,-100-(sqrt(50*50-25*25)-(25.0/tan(M_PI/3)))));
+	line2->addPoint(Point(-125,-100+(25.0/tan(M_PI/3))));
 
+	//shape 3
+	line3->addPoint(Point(75,100+(25.0/tan(M_PI/3))));
+	line3->addPoint(Point(125, 100+(25.0/tan(M_PI/3))));
+	line3->addPoint(Point(100,100-(sqrt(50*50-25*25)-(25.0/tan(M_PI/3)))));
+	line3->addPoint(Point(75,100+(25.0/tan(M_PI/3))));
 
 
 	// add this line to the shape
 	shape->addLine(line);
     shape2->addLine(line2);
+	shape3->addLine(line3);
 	// add the shape to the window
 	window.addShape(shape);
     window.addShape(shape2);
+	window.addShape(shape3);
 	// notice the use of . notation for the window
 	// and contrast with the use of -> notation for the shape and line
 
