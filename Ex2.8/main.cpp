20d801417933b2f4a51e2c5d2ec3998d447cac99
ap16032 <a.dev@se16.qmul.ac.uk>
2018-02-16 16:15:46 +0000

    (lab) Section C/ Worksheet 2/ HW/ Exercise 8 completed

--- Ex2.8/main.cpp
+++ Ex2.8/main.cpp
@@ -16,11 +16,15 @@ int main(int argc, char * argv[]) {
 
 	// Add code here to draw things
 	Shape * shape = new Shape(250,250,0);
+    Shape * shape2 = new Shape(250,250,45);
 	shape->setRotationSpeed(2); // initial rotation speed in degrees
+    shape2->setRotationSpeed(-2);
 	// make a new line
 	Line * line = new Line();
+    Line * line2 = new Line();
 	// allowed colours are specified in the enum in Colours.hpp
 	line->setColor(getColor(BLACK));
+    line2->setColor(getColor(RED));
 	// add a point to the line
 	// these points are all relative to the centre of the Shape
 	line->addPoint(Point(-100,-100));
@@ -28,10 +32,17 @@ int main(int argc, char * argv[]) {
 	line->addPoint(Point(100,100));
 	line->addPoint(Point(-100,100));
 	line->addPoint(Point(-100,-100));
+    line2-> addPoint(Point(-50,-50));
+    line2-> addPoint(Point(50,-50));
+    line2-> addPoint(Point(50,50));
+    line2-> addPoint(Point(-50,50));
+    line2-> addPoint(Point(-50,-50));
 	// add this line to the shape
 	shape->addLine(line);
+    shape2->addLine(line2);
 	// add the shape to the window
 	window.addShape(shape);
+    window.addShape(shape2);
 	// notice the use of . notation for the window
 	// and contrast with the use of -> notation for the shape and line
 
