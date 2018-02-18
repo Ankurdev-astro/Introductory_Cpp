acf1f77fcf69dd792e224fc599aab7a0a284e0df
Ankur Dev <a.dev@se16.qmul.ac.uk>
2018-02-18 12:36:10 +0000

    comments added

--- Ex2.9/main.cpp
+++ Ex2.9/main.cpp
@@ -15,57 +15,56 @@ int main(int argc, char * argv[]) {
 	SPA::Window window(500,500,"My Test",24);
 	Fl::visual(FL_DOUBLE | FL_INDEX);
 
-	// Add code here to draw things
-	Shape * shape = new Shape(250,250,0);
-    Shape * shape2 = new Shape(250,250,0);
-	Shape * shape3 = new Shape(250,250,0);
+	Shape * shape = new Shape(250,250,0); //creates new Shape object rotating about (250,250)
+    Shape * shape2 = new Shape(250,250,0); //creates new Shape object rotating about (250,250)
+	Shape * shape3 = new Shape(250,250,0); //creates new Shape object rotating about (250,250)
 	shape->setRotationSpeed(2); // initial rotation speed in degrees
-    shape2->setRotationSpeed(4);
-	shape3->setRotationSpeed(-4);
+    shape2->setRotationSpeed(4); //set initial rotation to 4
+	shape3->setRotationSpeed(-4); //set initial rotation to -4
 	// make a new line
 	Line * line = new Line();
     Line * line2 = new Line();
 	Line * line3 = new Line();
 	// allowed colours are specified in the enum in Colours.hpp
-	line->setColor(getColor(BLACK));
-    line2->setColor(getColor(RED));
-	line3->setColor(getColor(BLUE));
+	line->setColor(getColor(BLACK));  // Set line to Black
+    line2->setColor(getColor(RED));  //Set line2 to Red
+	line3->setColor(getColor(BLUE)); //Set line 3 to Blue
 	// add a point to the line
 	// these points are all relative to the centre of the Shape
+
+    //shape  = Black square
+    // Added these points to make square of length 200 centred on and rotating around (250,250)
 	line->addPoint(Point(-100,-100));
 	line->addPoint(Point(100,-100));
 	line->addPoint(Point(100,100));
 	line->addPoint(Point(-100,100));
 	line->addPoint(Point(-100,-100));
 
-    //shape 2
+    //shape 2 = Red triangle
+    // Added these point to make a triangle
+    // side length 50 initially centred on (150,150) and rotating around (250,250)
     line2->addPoint(Point(-125,-100+(25.0/tan(M_PI/3))));
     line2->addPoint(Point(-75, -100+(25.0/tan(M_PI/3))));
     line2->addPoint(Point(-100,-100-(sqrt(50*50-25*25)-(25.0/tan(M_PI/3)))));
 	line2->addPoint(Point(-125,-100+(25.0/tan(M_PI/3))));
 
-	//shape 3
+	//shape 3 = Blue triangle
+    // side length 50 initially centred on (350,350) and rotating around (250,250)
 	line3->addPoint(Point(75,100+(25.0/tan(M_PI/3))));
 	line3->addPoint(Point(125, 100+(25.0/tan(M_PI/3))));
 	line3->addPoint(Point(100,100-(sqrt(50*50-25*25)-(25.0/tan(M_PI/3)))));
 	line3->addPoint(Point(75,100+(25.0/tan(M_PI/3))));
 
 
-	// add this line to the shape
+	// add these lines to the shape
 	shape->addLine(line);
     shape2->addLine(line2);
 	shape3->addLine(line3);
-	// add the shape to the window
+	// add the shapes to the window
 	window.addShape(shape);
     window.addShape(shape2);
 	window.addShape(shape3);
-	// notice the use of . notation for the window
-	// and contrast with the use of -> notation for the shape and line
 
-	// add your code here!
-
-
-	////////
 	// display the window
 	window.show(argc,argv);
 	// run the FLTK event loop - this allows you to move the window around etc
