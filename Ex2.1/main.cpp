a88417a61a2c7909bb1a0de16162f361735e6f54
Ankur Dev <a.dev@se16.qmul.ac.uk>
2018-02-18 10:29:52 +0000

    comments updated

--- Ex2.1/main.cpp
+++ Ex2.1/main.cpp
@@ -1,16 +1,10 @@
-/*
- * main.cpp
- *
- *  Created on: 18 Dec 2014
- *      Author: apw253
- */
-
 #include "Window.h"
 #define _USE_MATH_DEFINES
 #include <cmath>
 
+//Section C/ HW/Worksheet 1/ Ex 1
 
-float rad(float deg)
+float rad(float deg)        // Defining function to convert degrees to radians
     {
         float r = (deg*M_PI)/180;
         return r;
@@ -21,54 +15,41 @@ int main(int argc, char * argv[]) {
 // the top left corner of the window is (0,0)
 	SPA::Window window(500,500,"My Test");
 
-	// Add code here to draw things
-
-	/*
-	 * Here are some of the options
-	 * window.startNewLine(float x, float y) start a new line from location (x,y)
-	 * window.penSetAngle(float angle) to set the initial ang of the pen in degrees
-	 * window.penRotateDegrees(float angle) rotate the pen clockwise in degrees
-	 * window.penForward(float distance) move the pen forward
-	 * window.stopLine() to stop the current line
-*/
-
-	// uncomment the following lines to see a working example of how to draw lines
-
-    // for 2 lines
-	window.startNewLine(250,0);
+// for 2 lines
+	window.startNewLine(250,0);    //defines start position of line 1
     window.penRotateDegrees(90);
-    //window.penSetAngle(0);
-	window.penForward(500);
-	//window.penRotateDegrees(90);
-	//window.penForward(250);
+    window.penForward(500);
 	window.stopCurrentLine();
-    window.startNewLine(0,250);
+    window.startNewLine(0,250);     //defines start position of line 2
     window.penSetAngle(0);
     window.penForward(500);
     window.stopCurrentLine();
 // for triangle
     float x,y;
+    //calculating (x,y) position of top-left vertex of triangle
     x = (500-200.0)/2.0;
     y = 250 - 100/tan(rad(60));
     int i;
     window.startNewLine(x,y);
-    for (i=0;i<3;i++)
+    for (i=0;i<3;i++)                  //loop to draw 3 sides of triangle
         {
             window.penForward(200);
             window.penRotateDegrees(120);
         }
     window.stopCurrentLine();
+
 // for heptagon
     float r, theta, n;
-    n = 7;
+    n = 7;  //heptagon has 7 sides
     theta = float((n-2)*180)/n;
+    //calculating (x,y) position of top-left vertex of heptagon
     r = 200/(2*sin(M_PI/n));
     x = 250 - r*cos(rad(theta/2));
     y = 250 - r*sin(rad(theta/2));
     window.startNewLine(x,y);
     window.penSetAngle(0);
 
-   for (i=0;i<7;i++)
+   for (i=0;i<7;i++)        //loop to draw 7 sides of heptagon
     {
         window.penForward(200);
         window.penRotateDegrees(180-theta);
