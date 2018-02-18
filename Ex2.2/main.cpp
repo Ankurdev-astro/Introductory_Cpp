6c6639dceb10068afed5c2ce3ae7abd8f71fe0db
Ankur Dev <a.dev@se16.qmul.ac.uk>
2018-02-18 11:07:41 +0000

    comments added

--- Ex2.2/main.cpp
+++ Ex2.2/main.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 // Section C/ Worksheet 1/HW/ Exercise 2
 
-float rad(float deg)
+float rad(float deg)            //function to convert degrees to radian
 {
     float r = (deg*M_PI)/180;
     return r;
@@ -18,27 +18,28 @@ int main(int argc, char * argv[]) {
     float n,theta,r,x,y;
     cout << "Enter the number of sides of shape: \n ";
     do {
-        cin >> nSides;
-        if (cin.fail() == 1)
+        cin >> nSides;  // user input stored in nSides variable
+        if (cin.fail() == 1)  // if the fail flag of stream is raised, the program is terminated
             exit(0);
-    }while(nSides<=2);
+    }while(nSides<=2);  // checks if sides entered is greater than 2, else keep asking
 
     cout << "Enter the side length: \n ";
     do {
         cin >> sideLength;
-        if (cin.fail() == 1)
+        if (cin.fail() == 1)    // if the fail flag of stream is raised, the program is terminated
             exit(0);
-    }while(sideLength<=0);
+    }while(sideLength<=0);  // checks if side length entered is greater than 0, else keep asking
 
     n = nSides;
-    theta = float((n-2)*180)/n;
+    theta = float((n-2)*180)/n; // calculating interior angle of a polygon
     r = sideLength/(2*sin(M_PI/n));
+    //calculation of top left vertex (x,y) position of polygon
     x = 250 - r*cos(rad(theta/2));
     y = 250 - r*sin(rad(theta/2));
     window.startNewLine(x,y);
     window.penSetAngle(0);
 
-    for (int i=0;i<n;i++)
+    for (int i=0;i<n;i++)   //loop to make n sides of polygon
     {
         window.penForward(sideLength);
         window.penRotateDegrees(180-theta);
